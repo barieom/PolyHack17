9b8a3b2da10109b524fbdb79a69e569a017f2214
moose <moose@wr-130-64-217-130.medford.tufts.edu>
2017-10-13 21:55:38 -0400

    Push project

---
+++ castle_storm/Assets/Castle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CastleModule;
+using Health;
+
+public class Castle : MonoBehaviour {
+	public Health castleHealth;
+
+	List<Object> modules;
+	List<CastleModule_TopModule> top_mod;
+	List<CastleModule_CoreModule> core_mod;
+	List<CastleModule_BaseModule> base_mod;
+
+	// Use this for initialization
+	void Start () {
+		//Build castle on screen
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (castle_health.isDead ()) {
+			//Trigger game over sequence
+		}
+	}
+
+	void movePlayer(Player pl, uint module_index, uint destination_index)
+	{
+
+	}
+
+	void swapModule<T>(List<T> module_list, uint module_index, T replacement_module)
+	{
+		if (module_list [module_index].players.Count == 0) {
+			if (!module_list [module_index].onCooldown ()) {
+				module_list.RemoveAt (module_index);
+				module_list.Insert (module_index, replacement_module);
+			} else {
+				//Trigger "Module to be replaced is on cooldown." dialogue.
+			}
+		} else {
+			//Trigger "Module to be replaced contains players." dialgoue.
+		}
+	}
+}
+
---
+++ castle_storm/Assets/CastleModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleModule {
+	uint cooldown;
+	uint cooldown_start_time = 0;
+	public List<Player> players;
+	public bool onCooldown()
+	{
+
+	}
+}
+
+public class CastleModule_TopModule : CastleModule {
+        
+}
+	
+public class CastleModule_CoreModule : CastleModule {
+
+}
+
+public class CastleModule_BaseModule : CastleModule {
+        
+}
